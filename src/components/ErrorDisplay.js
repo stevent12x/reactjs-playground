c3ba1e47a8d3f5bade684af4b748b5449092e883
Steven Turner <steven.turner12x@gmail.com>
2019-10-14 12:34:16 -0500

    10/14 am commit

--- src/components/ErrorDisplay.js
+++ src/components/ErrorDisplay.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 // Incorporating inline style using JSX //
+// !This kind of styling is NOT considered best practice! //
 const ErrorDisplay = ({message}) => (
 	<div style={{
 		color: 'red',
@@ -11,3 +12,15 @@ const ErrorDisplay = ({message}) => (
 );
 
 export default ErrorDisplay;
+
+const ConditionalError = ({message}) => (
+	<div style={{
+		color: Math.random() < 0.5 ? 'green' : 'blue',
+		backgroundColor: 'yellow',
+		textAlign: 'center'
+	}}>
+		{message}
+	</div>
+)
+export {ConditionalError};
+
--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {render} from './App'
 import {Components, RandomValue} from './Components.js'
-import ErrorDisplay from "./components/ErrorDisplay";
+import ErrorDisplay, {ConditionalError} from "./components/ErrorDisplay";
 import * as serviceWorker from './serviceWorker';
-
 
 setInterval(render, 1000);
 ReactDOM.render(<Components label="Save" />, document.getElementById('mountNode'));
 ReactDOM.render(<RandomValue/>, document.getElementById('random'));
 ReactDOM.render(<ErrorDisplay message="Not today, son" />, document.getElementById('error'));
+ReactDOM.render(<ConditionalError message="This should be different from time to time" />, document.getElementById('conditional') )
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
